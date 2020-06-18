8e059c8f27fba9edfedcc1f6a31da628cfa054ae
fatesigner <fatesigner@gmail.com>
2020-06-18 14:34:53 +0800

    feat: 添加.prettierrc.js至dist目录

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -10,12 +10,7 @@ Gulp.task('build', async function () {
   const ENV = require('../env')();
   await Gulp.src([Path.join(ENV.srcPath, '**/*'), Path.join(ENV.srcPath, '**/.*')]).pipe(Gulp.dest(ENV.outputPath));
   // copy npm publish files to output
-  await Gulp.src(['package.json', 'README.md'].map((x) => Path.join(ENV.rootPath, x))).pipe(Gulp.dest(ENV.outputPath));
+  await Gulp.src(['.prettierrc.js', 'package.json', 'README.md'].map((x) => Path.join(ENV.rootPath, x))).pipe(
+    Gulp.dest(ENV.outputPath)
+  );
 });
-
-var s = [
-  {
-    value: 'upd',
-    name: 'A new feature.'
-  }
-];
